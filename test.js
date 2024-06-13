789ab7b6fb1c28382ddd26041f8657d70f1fbadb
Kelechi Daniel Onwumere <135511204+keldan24@users.noreply.github.com>
2024-06-13 15:51:05 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,1 @@
-function fibonacci(n) {
-    if (n <= 0) {
-        return 0;
-    } else if (n === 1) {
-        return 1;
-    } else {
-        return fibonacci(n - 1) + fibonacci(n - 2);
-    }
-}
-
-// Test cases
-console.log(fibonacci(0));  // 0
-console.log(fibonacci(1));  // 1
-console.log(fibonacci(4));  // 5
-console.log(fibonacci(10)); // 55
+null
